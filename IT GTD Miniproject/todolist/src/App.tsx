c047774394801100392db06503069097b99c8d0b
Enzio <zio.ds21@gmail.com>
2026-01-03 19:28:03 +0800

    update again

--- IT GTD Miniproject/todolist/src/App.tsx
+++ IT GTD Miniproject/todolist/src/App.tsx
@@ -13,21 +13,22 @@ function App() {
     localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks])
 
-  return (
-    <div className="bg-gray-200 min-h-screen">
-      <h1 className='text-4xl text-center font-bold p-4'>Todo List</h1>
+ return (
+  <div className="bg-gray-200 min-h-screen">
+    <h1 className='text-4xl text-center font-bold p-4'>Todo List</h1>
 
-      <AddTaskForm tasks={tasks} setTasks={setTasks}/>
-      <TaskViewList tasks={tasks} setTasks={setTasks}/>
+    <AddTaskForm tasks={tasks} setTasks={setTasks}/>
+    <TaskViewList tasks={tasks} setTasks={setTasks}/>
 
-      {/* ⬇️ BUTTON AT BOTTOM */}
-      <div className="text-center py-6">
-        <a href="https://it-gtd-xxviii-miniproject.vercel.app/index.html">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded">
-            Back to Main Page
-          </button>
-        </a>
-      </div>
+    {/* ⬇️ BUTTON AT BOTTOM */}
+    <div className="text-center py-6">
+      <a href="https://it-gtd-xxviii-miniproject.vercel.app/index.html">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">
+          Back to Main Page
+        </button>
+      </a>
+    </div>
   </div>
-
+);
+}   
 export default App
